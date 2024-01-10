cf6e7a6c1a5844424a9060ec5d076cc154219a40
Matheus Pereira Lima <132246877+Plimacs@users.noreply.github.com>
2024-01-10 17:31:27 -0300

    close#12

--- 3Calculo-De-Horas-Necessárias.js
+++ 3Calculo-De-Horas-Necessárias.js
@@ -1,7 +1,11 @@
 const receber = require('prompt-sync')({sigint: false});
 
-let carga = receber(`Digite em toneladas a carga total a ser pavimentada: `)
+let carga = receber(`Digite em toneladas a carga total a ser pavimentada (ex: "1.4" = 1t e 400kg): `)
+let cargaCaminhao = receber('Digite a capacidade do seu caminhão em toneladas (ex: "1.4" = 1t e 400kg): ')
+let mediaTempo = receber('Digite o tempo nessesário para descarregar um caminhão (ex: "1.4" = 1h e 40min): ')
+
 console.log(`Você digitou ${carga} t toneladas.`)
+
 let continuar = receber(`Digite "y" para continuar, ou "n" para editar: `)
 
 if (continuar.toLowerCase() === 'n'){
@@ -13,8 +17,8 @@ let dias = 0, horas = 0;
 
 while(cargaCalc > 0){
 
-    cargaCalc = cargaCalc - 1
-    horas = horas + 0.15
+    cargaCalc = cargaCalc - cargaCaminhao
+    horas = horas + mediaTempo
 
     if (horas >= 24){
 
