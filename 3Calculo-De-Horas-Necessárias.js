fbe9a09c7fba980bed122dcb460a7716f8f06592
Matheus Pereira Lima <132246877+Plimacs@users.noreply.github.com>
2024-01-10 17:20:34 -0300

    loop reformulado close #11

--- 3Calculo-De-Horas-Necessárias.js
+++ 3Calculo-De-Horas-Necessárias.js
@@ -8,29 +8,19 @@ if (continuar.toLowerCase() === 'n'){
     carga = receber(`Digite em toneladas a carga total a ser pavimentada: `)
 }
 
-// reformular todo o loop
 let cargaCalc = carga
 let dias = 0, horas = 0;
 
-while(cargaCalc != 0){
-    if (cargaCalc != 0){
+while(cargaCalc > 0){
 
-        cargaCalc = cargaCalc - 1
-        horas = horas + 0.15
+    cargaCalc = cargaCalc - 1
+    horas = horas + 0.15
 
-    }else{
-        break
+    if (horas >= 24){
+
+        dias = dias +1
+        horas = horas -24
     }
 }
 
-if(horas >= 24){
-
-    dias = horas
-    dias = dias / 24;
-    
-}
-
-dias = dias + 1;
-
-// reformular resposta mais precisa
 console.log(`você vai precisar de ${dias.toFixed(0)} dias ou ${horas.toFixed(2)} horas para pavimentar ${carga}t toneladas`);
